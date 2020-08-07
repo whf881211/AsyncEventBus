6b0f910e28a6617b84cea51be6ca3d79a7d5b5fc
ryanhfwang <ryanhfwang@tencent.com>
2020-08-07 20:21:12 +0800

    —init

---
+++ EventBusExample/EventBusExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EventBusExample
+//
+//  Created by ryanhfwang on 2020/8/7.
+//  Copyright © 2020 ryanhfwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EventBusExample/EventBusExample/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  EventBusExample
+//
+//  Created by ryanhfwang on 2020/8/7.
+//  Copyright © 2020 ryanhfwang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
